d361430b5dc7951aecd5e017ffe78508ad3d45c3
Max Gaponov <gaponovmaxev@gmail.com>
2020-05-25 18:58:37 +0300

    tabs to spaces

--- rates.py
+++ rates.py
@@ -5,21 +5,21 @@ from config import BASE_CURRENCY, RATES_API_KEY, RATES_API_URL, CURRENCIES
 
 
 def convert_rates_to_base(rates):
-	result = {}
-	for cur in CURRENCIES:
-		result[cur] = rates[BASE_CURRENCY] / rates[cur]
-	return result
+    result = {}
+    for cur in CURRENCIES:
+        result[cur] = rates[BASE_CURRENCY] / rates[cur]
+    return result
 
 
 def make_api_request():
-	params = {
-		'app_id': RATES_API_KEY
-	}
-	return requests.get(RATES_API_URL, params=params)
+    params = {
+        'app_id': RATES_API_KEY
+    }
+    return requests.get(RATES_API_URL, params=params)
 
 
 @ttl_cache(maxsize=1, ttl=60 * 60)
 def get_rates():
-	response = make_api_request()
-	rates = json.loads(response.text)['rates']
-	return convert_rates_to_base(rates)
+    response = make_api_request()
+    rates = json.loads(response.text)['rates']
+    return convert_rates_to_base(rates)
